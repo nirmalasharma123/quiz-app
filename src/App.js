30a6944490bb7b86619d8857eab34226f53c3af1
nirmalasharma123 <jassusharma159@gmail.com>
2023-05-03 18:44:33 +0530

    quize

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from "react";
+import React from "react";
+import { questions } from "./quation";
 
-function App() {
+
+
+export default function App() {
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const [score, setScore] = React.useState(0);
+  const [showScore, setShowScore] = React.useState(false);
+
+  const handleClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
-      );
-}
+    <div className="app">
+      {showScore ? (
+        <section className="showScore-section">
+          Your score is {score} out of {questions.length}
+        </section>
+      ) : (
+        <>
+          <section className="question-section">
+            <h1>
+              Question {currentQuestion + 1}/{questions.length}
+            </h1>
+            <p>{questions[currentQuestion].questionText}</p>
+          </section>
 
-export default App;
+          <section className="answer-section">
+            {questions[currentQuestion].answerOptions.map((item) => (
+              <button onClick={() => handleClick(item.isCorrect)}>
+                {item.answerText}
+              </button>
+            ))}
+          </section>
+        </>
+      )}
+    </div>
+  );
+}
---
+++ src/quation.js
@@ -0,0 +1,38 @@
+export const questions = [
+    {
+      questionText: "What is the capital of INdia?",
+      answerOptions: [
+        { answerText: "Gujarat", isCorrect: false },
+        { answerText: "Rajasthan", isCorrect: false },
+        { answerText: "Delhi", isCorrect: true },
+        { answerText: "Bangalore", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Who is CEO of Tesla?",
+      answerOptions: [
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "The iPhone was created by which company?",
+      answerOptions: [
+        { answerText: "Apple", isCorrect: true },
+        { answerText: "Intel", isCorrect: false },
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Microsoft", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "How many Harry Potter books are there?",
+      answerOptions: [
+        { answerText: "1", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
+      ],
+    },
+  ];
